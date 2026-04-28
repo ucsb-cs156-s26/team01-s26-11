78eb18093b4dc8ba2cf177582f85f823bac349e2
Brian Hu <bjhu834@ucsb.edu>
2026-04-28 14:34:24 -0700

    Removed File

--- src/main/java/edu/ucsb/cs156/example/repositories/HelpRequest.java
+++
@@ -1,9 +0,0 @@
-package edu.ucsb.cs156.example.repositories;
-
-import edu.ucsb.cs156.example.entities.HelpRequest;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-/** The HelpRequestRepository is a repository for HelpRequest entities. */
-@Repository
-public interface HelpRequestRepository extends CrudRepository<HelpRequest, Long> {}
